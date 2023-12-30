0e184840df889552eb181fad732cfef8c6d2c1ce
Jaeheon-So <sjh-98@naver.com>
2023-12-31 01:44:50 +0900

    Add number of items in cart in header's cart logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 import { GrCart } from "react-icons/gr";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { cartState } from "../store/cartAtom";
 
 const Header = () => {
+  const cartList = useRecoilValue(cartState);
+
   return (
     <Container>
       <Link to="/">
@@ -10,6 +14,9 @@ const Header = () => {
       </Link>
       <Link to="/cart">
         <GrCart />
+        <CartCount>
+          <span>{cartList.length}</span>
+        </CartCount>
       </Link>
     </Container>
   );
@@ -25,12 +32,31 @@ const Container = styled.header`
     width: 40px;
     height: 40px;
   }
+
+  a {
+    position: relative;
+  }
 `;
 
 const HomeLogo = styled.div`
   color: #4e2e1d;
   font-weight: bold;
   font-size: 30px;
+`;
+
+const CartCount = styled.div`
+  position: absolute;
+  top: -3px;
+  right: -5px;
+  width: 19px;
+  height: 19px;
+  text-align: center;
+  border-radius: 50%;
+  background-color: orange;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Header;
